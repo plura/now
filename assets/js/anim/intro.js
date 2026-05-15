58c425be32f28c8ccc941d7fc91d5525ed77f438
trpsimoes <trpsimoes@gmail.com>
2026-05-15 21:00:58 +0100

    Split O div coords into getCoordsFromU/getCoordsFromMain; add ResizeObserver for U phase

--- assets/js/anim/intro.js
+++ assets/js/anim/intro.js
@@ -1,9 +1,8 @@
 import { animatePluraLogoIntro, getCapOverhang } from './logo.js';
 
-function positionODiv(oDiv, logo) {
-  const uPaths = [...logo.querySelectorAll('[id^="plura-anim-l-u-"]')]
+function getCoordsFromU(logo) {
+  const uPaths     = [...logo.querySelectorAll('[id^="plura-anim-l-u-"]')]
     .filter(el => !el.id.endsWith('-x'));
-
   const capOverhang = getCapOverhang(uPaths[0]);
   const rects       = uPaths.map(el => el.getBoundingClientRect());
 
@@ -12,11 +11,20 @@ function positionODiv(oDiv, logo) {
   const right  = Math.max(...rects.map(r => r.right))  + capOverhang;
   const bottom = Math.max(...rects.map(r => r.bottom)) + capOverhang;
 
+  return { top, left, width: right - left, height: bottom - top };
+}
+
+function getCoordsFromMain() {
+  const { top, left, width, height } = document.querySelector('main').getBoundingClientRect();
+  return { top, left, width, height };
+}
+
+function positionODiv(oDiv, coords) {
   Object.assign(oDiv.style, {
-    top:    `${top}px`,
-    left:   `${left}px`,
-    width:  `${right - left}px`,
-    height: `${bottom - top}px`,
+    top:    `${coords.top}px`,
+    left:   `${coords.left}px`,
+    width:  `${coords.width}px`,
+    height: `${coords.height}px`,
   });
 }
 
@@ -31,7 +39,10 @@ export function runIntroSequence() {
   const logo = document.querySelector('#plura-intro svg');
   const oDiv = createExpandingO();
 
-  positionODiv(oDiv, logo);
+  positionODiv(oDiv, getCoordsFromU(logo));
+
+  const observer = new ResizeObserver(() => positionODiv(oDiv, getCoordsFromU(logo)));
+  observer.observe(document.body);
 
   const master = gsap.timeline({
     // onComplete: () => document.querySelector('#plura-intro').remove(),
@@ -39,7 +50,7 @@ export function runIntroSequence() {
 
   master.add(animatePluraLogoIntro(logo));
 
-  // TODO: animate oDiv expanding to match <main> bounds, then remove
+  // TODO: on intro complete, disconnect observer and tween oDiv to getCoordsFromMain()
 
   return master;
 }
