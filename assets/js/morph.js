2b3cf875bf8c26699959ee10bbc99cb7e77e6de1
trpsimoes <trpsimoes@gmail.com>
2026-05-19 18:29:51 +0100

    Switch morph animation to transform x/y for better compositing

--- assets/js/morph.js
+++ assets/js/morph.js
@@ -11,18 +11,21 @@ export function openMorph(mainEl, morphEl, fromRect, toSize) {
   const toX = (window.innerWidth  - toSize.width)  / 2;
   const toY = (window.innerHeight - toSize.height) / 2;
 
+  // Anchor left/top once (no animation cost), then move with transform
   gsap.set(morphEl, {
     left:   fromRect.x,
     top:    fromRect.y,
     width:  fromRect.width,
     height: fromRect.height,
+    x: 0,
+    y: 0,
   });
 
   mainEl.classList.add('active');
 
   gsap.to(morphEl, {
-    left:   toX,
-    top:    toY,
+    x:      toX - fromRect.x,
+    y:      toY - fromRect.y,
     width:  toSize.width,
     height: toSize.height,
     duration: 0.45,
@@ -35,8 +38,8 @@ export function closeMorph(mainEl, morphEl) {
   if (!toRect) return;
 
   gsap.to(morphEl, {
-    left:   toRect.x,
-    top:    toRect.y,
+    x:      0,
+    y:      0,
     width:  toRect.width,
     height: toRect.height,
     duration: 0.45,
