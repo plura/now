0ede68d222ffc480ffd6e8b8532d0d491ff35e63
trpsimoes <trpsimoes@gmail.com>
2026-05-28 23:29:19 +0100

    Style projects carousel item header, body, and category

--- src/assets/js/projects/carousel-item.js
+++ src/assets/js/projects/carousel-item.js
@@ -6,8 +6,8 @@ import { buildProjectMeta } from '../projects.js';
 export function createProjectsCarouselItem(project) {
   return el('div', { class: 'plura-projects-carousel-item plura-panel' },
     el('div', { class: 'plura-projects-carousel-item-header plura-panel-header' },
-      el('span', { class: 'plura-projects-carousel-item-category', text: project.category.label }),
       el('h2',  { class: 'plura-projects-carousel-item-title plura-panel-title', text: project.title }),
+      el('span', { class: 'plura-projects-carousel-item-category', text: project.category.label }),
       buildProjectMeta(project)
     ),
     project.summary && el('div', { class: 'plura-projects-carousel-item-body plura-panel-body' },
