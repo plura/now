a2c6e6f464df1d03cfba9b557b7f8ed6a16a471a
trpsimoes <trpsimoes@gmail.com>
2026-06-03 22:48:25 +0100

    Wire gallery component into carousel item; lazy mount on media presence

--- src/assets/js/projects/carousel-item.js
+++ src/assets/js/projects/carousel-item.js
@@ -3,9 +3,10 @@
 import { el } from '../utils.js';
 import { buildMeta } from './meta.js';
 import { basePath } from '../config.js';
+import { createGallery } from '../../components/gallery/gallery.js';
 
 export function createCarouselItem(project) {
-  return el('div', { class: 'plura-projects-carousel-item plura-panel' },
+  const item = el('div', { class: 'plura-projects-carousel-item plura-panel' },
     el('div', { class: 'plura-projects-carousel-item-header plura-panel-header' },
       el('h2',  { class: 'plura-projects-carousel-item-title plura-panel-title', text: project.title }),
       el('span', { class: 'plura-projects-carousel-item-category', text: project.category.label }),
@@ -13,7 +14,17 @@ export function createCarouselItem(project) {
     ),
     project.summary && el('div', { class: 'plura-projects-carousel-item-body plura-panel-body' },
       el('p', { class: 'plura-projects-carousel-item-desc', text: project.summary })
-    ),
-    el('div', { class: 'plura-projects-carousel-item-gallery' })
+    )
   );
+
+  if (project.media.length) {
+    const mediaItems = project.media.map(m =>
+      `${basePath}/assets/media/projects/${project.slug}/${m.file}`
+    );
+    const { root } = createGallery(document.createElement('div'), mediaItems);
+    root.classList.add('plura-projects-carousel-item-gallery');
+    item.appendChild(root);
+  }
+
+  return item;
 }
